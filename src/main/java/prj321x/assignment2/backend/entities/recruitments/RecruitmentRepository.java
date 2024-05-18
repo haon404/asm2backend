5dfd17aa773371d778366e91a848964b7e01a420
haon404 <haonafx20622@funix.edu.vn>
2024-05-18 20:15:41 +0900

    finish TODO: Change to recruitment based on category

--- src/main/java/prj321x/assignment2/backend/entities/recruitments/RecruitmentRepository.java
+++ src/main/java/prj321x/assignment2/backend/entities/recruitments/RecruitmentRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, UUID> {
-    @Query("select r from Recruitment r join r.categories rc where rc.id = :category order by size(r.recruitmentApplies)")
+    @Query("select r from Recruitment r join r.categories rc " +
+            "where rc.id = :category " +
+            "order by size(r.recruitmentApplies)")
     Optional<Recruitment> findMostAppliedRecruitmentByCategory(@Param("category") UUID category);
 }
